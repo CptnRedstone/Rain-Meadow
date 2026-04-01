efc69a576986b3ff5288d7871c0d96820c8c0963
CptnRedstone <duckpondaab@gmail.com>
2026-04-01 18:21:30 -0500

    - Fixed still being able to interact with other UI elements. - Fixed (inadvertantly) the selectedObject not returning to its previous position when the dialog is closed. - Dialog can now be closed with Throw or Esc.

--- Menu/Dialogs/SimpleDialogBoxNotify.cs
+++ Menu/Dialogs/SimpleDialogBoxNotify.cs
@@ -5,34 +5,45 @@ namespace RainMeadow
     /// A simple, self-contained dialog box that destroys itself upon pressing Continue. Automatically translates input.
     /// Merely displays text and can be cleared, it does NOT block mouse interaction with other UI elements.
     /// </summary>
-    public class SimpleDialogBoxNotify : DialogBoxNotify
+    public class SimpleDialogBoxNotify : Dialog //In order to temporarily disable other UI elements, we need to have a Dialog to pass into ShowDialog. DialogBox is somehow not a Dialog so we need a wrapper.
     {
-        private bool continueEverClicked;
-        public SimpleDialogBoxNotify(Menu.Menu menu, MenuObject owner, string dialogText, string buttonText = "CONTINUE", bool forceWrapping = true, bool disableTimeOut = true)
-            : base(menu, owner, dialogText, "", new(menu.manager.rainWorld.options.ScreenSize.x / 2f - 240f + (1366f - menu.manager.rainWorld.options.ScreenSize.x) / 2f, 224f), new(480f, 320f), forceWrapping)
+        public SimpleDialogBoxNotifyActualDialogBox actualDialog;
+        public SimpleDialogBoxNotify(Menu.Menu menu, MenuObject owner, string dialogText, string buttonText = "CONTINUE", bool forceWrapping = true, bool disableTimeOut = true) : base(menu.manager)
         {
-            owner.subObjects.Add(this);
-            continueEverClicked = false;
-            descriptionLabel.text = menu.LongTranslate(descriptionLabel.text);
-            continueButton.menuLabel.text = menu.Translate(buttonText);
-            timeOut = disableTimeOut ? 0f : timeOut;
+            actualDialog = new(this, dialogPage, dialogText, buttonText, forceWrapping, disableTimeOut);
+            menu.manager.ShowDialog(this);
         }
-        public override void Update()
+        public class SimpleDialogBoxNotifyActualDialogBox : DialogBoxNotify
         {
-            base.Update();
-            if (continueButton.buttonBehav.clicked)
+            private bool continueEverClicked;
+            private bool escEverClicked;
+            public SimpleDialogBoxNotifyActualDialogBox(Menu.Menu menu, MenuObject owner, string dialogText, string buttonText = "CONTINUE", bool forceWrapping = true, bool disableTimeOut = true)
+                : base(menu, owner, dialogText, "", new(menu.manager.rainWorld.options.ScreenSize.x / 2f - 240f + (1366f - menu.manager.rainWorld.options.ScreenSize.x) / 2f, 224f), new(480f, 320f), forceWrapping)
             {
-                continueEverClicked = true;
+                owner.subObjects.Add(this);
+                continueEverClicked = false;
+                escEverClicked = false;
+                descriptionLabel.text = menu.LongTranslate(descriptionLabel.text);
+                continueButton.menuLabel.text = menu.Translate(buttonText);
+                timeOut = disableTimeOut ? 0f : timeOut;
             }
-            else if (continueEverClicked && !continueButton.buttonBehav.clicked)
+            public override void Update()
             {
-                RemoveSprites();
+                base.Update();
+                continueEverClicked |= continueButton.buttonBehav.clicked;
+                escEverClicked |= UnityEngine.Input.GetKey(UnityEngine.KeyCode.Escape); //NEVER use GetKeyDown outside of a GrafUpdate. It WILL drop 90% of your inputs.
+
+                if ((continueEverClicked && !continueButton.buttonBehav.clicked) || (menu.lastInput.thrw && !menu.input.thrw) || (escEverClicked && !UnityEngine.Input.GetKey(UnityEngine.KeyCode.Escape)))
+                {
+                    RemoveSprites();
+                }
             }
-        }
-        public override void RemoveSprites()
-        {
-            base.RemoveSprites();
-            owner.subObjects.Remove(this);
+            public override void RemoveSprites()
+            {
+                base.RemoveSprites();
+                owner.subObjects.Remove(this);
+                owner.menu.manager.dialog = null; //This seems dangeorus but it's how basegame does it.
+            }
         }
     }
 }
