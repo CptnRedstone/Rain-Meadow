54bc1607ef5bf9697a1642ea980582faec4cdfdd
CptnRedstone <duckpondaab@gmail.com>
2026-04-02 12:22:05 -0500

    Polish

--- Menu/Dialogs/SimpleDialogBoxNotify.cs
+++ Menu/Dialogs/SimpleDialogBoxNotify.cs
@@ -2,8 +2,7 @@
 namespace RainMeadow
 {
     /// <summary>
-    /// A simple, self-contained dialog box that destroys itself upon pressing Continue. Automatically translates input.
-    /// Merely displays text and can be cleared, it does NOT block mouse interaction with other UI elements.
+    /// A simple, self-contained dialog box that blocks other input and destroys itself upon pressing Continue. Automatically translates input text.
     /// </summary>
     public class SimpleDialogBoxNotify : Dialog //In order to temporarily disable other UI elements, we need to have a Dialog to pass into ShowDialog. DialogBox is somehow not a Dialog so we need a wrapper.
     {
@@ -17,12 +16,12 @@ namespace RainMeadow
         {
             private bool continueEverClicked;
             private bool escEverClicked;
+            private bool pauseEverClicked;
             public SimpleDialogBoxNotifyActualDialogBox(Menu.Menu menu, MenuObject owner, string dialogText, string buttonText = "CONTINUE", bool forceWrapping = true, bool disableTimeOut = true)
                 : base(menu, owner, dialogText, "", new(menu.manager.rainWorld.options.ScreenSize.x / 2f - 240f + (1366f - menu.manager.rainWorld.options.ScreenSize.x) / 2f, 224f), new(480f, 320f), forceWrapping)
             {
                 owner.subObjects.Add(this);
-                continueEverClicked = false;
-                escEverClicked = false;
+                (continueEverClicked, escEverClicked, pauseEverClicked) = (false, false, false);
                 descriptionLabel.text = menu.LongTranslate(descriptionLabel.text);
                 continueButton.menuLabel.text = menu.Translate(buttonText);
                 timeOut = disableTimeOut ? 0f : timeOut;
@@ -32,8 +31,12 @@ namespace RainMeadow
                 base.Update();
                 continueEverClicked |= continueButton.buttonBehav.clicked;
                 escEverClicked |= UnityEngine.Input.GetKey(UnityEngine.KeyCode.Escape); //NEVER use GetKeyDown outside of a GrafUpdate. It WILL drop 90% of your inputs.
+                pauseEverClicked |= RWInput.CheckPauseButton(0); //Why in the world is this not part of the input package?
 
-                if ((continueEverClicked && !continueButton.buttonBehav.clicked) || (menu.lastInput.thrw && !menu.input.thrw) || (escEverClicked && !UnityEngine.Input.GetKey(UnityEngine.KeyCode.Escape)))
+                if ((continueEverClicked    && !continueButton.buttonBehav.clicked)
+                    || (escEverClicked      && !UnityEngine.Input.GetKey(UnityEngine.KeyCode.Escape))
+                    || (pauseEverClicked    && !RWInput.CheckPauseButton(0))
+                    || (menu.lastInput.thrw && !menu.input.thrw))
                 {
                     RemoveSprites();
                 }
