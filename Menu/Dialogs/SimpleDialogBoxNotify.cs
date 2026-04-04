e240e1ad09d4884dae7fe0a9b26f6c69ef013ed9
CptnRedstone <duckpondaab@gmail.com>
2026-04-04 12:01:59 -0500

    More polish. Maybe need a refactor though...

--- Menu/Dialogs/SimpleDialogBoxNotify.cs
+++ Menu/Dialogs/SimpleDialogBoxNotify.cs
@@ -7,9 +7,9 @@ namespace RainMeadow
     public class SimpleDialogBoxNotify : Dialog //In order to temporarily disable other UI elements, we need to have a Dialog to pass into ShowDialog. DialogBox is somehow not a Dialog so we need a wrapper.
     {
         public SimpleDialogBoxNotifyActualDialogBox actualDialog;
-        public SimpleDialogBoxNotify(Menu.Menu menu, MenuObject owner, string dialogText, string buttonText = "CONTINUE", bool forceWrapping = true, bool disableTimeOut = true) : base(menu.manager)
+        public SimpleDialogBoxNotify(Menu.Menu menu, MenuObject owner, string dialogText, string buttonText = "CONTINUE", bool fullSize = true, bool forceWrapping = true, float canCloseDelay = 0f) : base(menu.manager)
         {
-            actualDialog = new(this, dialogPage, dialogText, buttonText, forceWrapping, disableTimeOut);
+            actualDialog = new(this, dialogPage, dialogText, buttonText, fullSize, forceWrapping, canCloseDelay);
             menu.manager.ShowDialog(this);
         }
         public class SimpleDialogBoxNotifyActualDialogBox : DialogBoxNotify
@@ -17,14 +17,14 @@ namespace RainMeadow
             private bool continueEverClicked;
             private bool escEverClicked;
             private bool pauseEverClicked;
-            public SimpleDialogBoxNotifyActualDialogBox(Menu.Menu menu, MenuObject owner, string dialogText, string buttonText = "CONTINUE", bool forceWrapping = true, bool disableTimeOut = true)
-                : base(menu, owner, dialogText, "", new(menu.manager.rainWorld.options.ScreenSize.x / 2f - 240f + (1366f - menu.manager.rainWorld.options.ScreenSize.x) / 2f, 224f), new(480f, 320f), forceWrapping)
+            public SimpleDialogBoxNotifyActualDialogBox(Menu.Menu menu, MenuObject owner, string dialogText, string buttonText = "CONTINUE", bool fullSize = true, bool forceWrapping = true, float canCloseDelay = 0f)
+                : base(menu, owner, dialogText, "", new(menu.manager.rainWorld.options.ScreenSize.x / 2f - 240f + (1366f - menu.manager.rainWorld.options.ScreenSize.x) / 2f, 224f), new(fullSize ? 480f : 240f, 320f), forceWrapping)
             {
                 owner.subObjects.Add(this);
                 (continueEverClicked, escEverClicked, pauseEverClicked) = (false, false, false);
                 descriptionLabel.text = menu.LongTranslate(descriptionLabel.text);
                 continueButton.menuLabel.text = menu.Translate(buttonText);
-                timeOut = disableTimeOut ? 0f : timeOut;
+                timeOut = canCloseDelay;
             }
             public override void Update()
             {
@@ -33,10 +33,11 @@ namespace RainMeadow
                 escEverClicked |= UnityEngine.Input.GetKey(UnityEngine.KeyCode.Escape); //NEVER use GetKeyDown outside of a GrafUpdate. It WILL drop 90% of your inputs.
                 pauseEverClicked |= RWInput.CheckPauseButton(0); //Why in the world is this not part of the input package?
 
-                if ((continueEverClicked    && !continueButton.buttonBehav.clicked)
-                    || (escEverClicked      && !UnityEngine.Input.GetKey(UnityEngine.KeyCode.Escape))
+                if (timeOut <= 0 && (
+                       (continueEverClicked && !continueButton.buttonBehav.clicked)
+                    || (escEverClicked      && !UnityEngine.Input.GetKey(UnityEngine.KeyCode.Escape)) //Esc *is* the keyboard pause button, but, keyboard might be rebound or disabled.
                     || (pauseEverClicked    && !RWInput.CheckPauseButton(0))
-                    || (menu.lastInput.thrw && !menu.input.thrw))
+                    || (menu.lastInput.thrw && !menu.input.thrw)))
                 {
                     RemoveSprites();
                 }
